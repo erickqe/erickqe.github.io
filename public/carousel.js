1648d71165eb3621982b3efc813b17791df7f0e3
Erick Quiros <51241534+ErickTechuso@users.noreply.github.com>
2024-04-13 14:20:53 -0600

    add carousel

---
+++ public/carousel.js
@@ -0,0 +1,122 @@
+function changeCarousel(val) {
+  const carousel = document.getElementById("items-carrusel");
+  const items = document.getElementById("items-content");
+  if (val.checked) {
+    carousel.classList.remove("hidden");
+    items.classList.add("hidden");
+  } else {
+    carousel.classList.add("hidden");
+    items.classList.remove("hidden");
+  }
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  function loadCarousel(title, data) {
+    const carousel = document.getElementById("items-carrusel");
+
+    let elements = "";
+    let indicators = "";
+    let index = 0;
+    for (let d of data) {
+      //<!-- Carousel wrapper -->
+      const projectHTML = `                    
+                    <!-- Item 1 -->
+                    <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                        <section class="flex justify-center items-center h-full">
+                            <div class="w-72 bg-gray-900 shadow-md rounded-xl duration-500 contrast-75 brightness-90 hover:scale-105 hover:shadow-xl hover:contrast-100 hover:brightness-100">
+                                <a href="${d.link}" target="_blank">
+                                    <img src="${d.image}" alt="Product" class="h-80 w-72 object-cover rounded-t-xl">
+                                    <div class="px-4 py-3 w-72">
+                                        <span class="text-gray-300 mr-3 uppercase text-xs">${d.brand}</span>
+                                        <p class="text-lg font-bold text-black text-white truncate block uppercase">${d.title}</p>
+                                        <div class="flex items-center">
+                                        <p class="text-lg font-semibold cursor-auto my-3 text-blue-400">${d.price}</p>                                          
+                                        </div>
+                                    </div>
+                                </a>
+                            </div>
+                        </section>
+                    </div>                  
+                    `;
+      elements += projectHTML;
+      indicators += `<button type="button" class="w-3 h-3 rounded-full" aria-current="true" aria-label="Slide 1" data-carousel-slide-to="${index}"></button>`;
+      index++;
+    }
+
+    //<!-- Slider Title -->
+    const ttl = `<div class="text-center pt-4">
+                    <h1 class="text-3xl uppercase text-4xl font-bold text-center mt-8 text-gray-700">${title}</h1>
+                 </div>`;
+
+    //<!-- Slider indicators -->
+    const indicatorsContent = `<div class="absolute z-30 flex -translate-x-1/2 bottom-5 left-1/2 space-x-3 rtl:space-x-reverse">${indicators}</div>`;
+    //<!-- Slider controls -->
+    const sliderControls = `
+            <button type="button" class="absolute top-0 start-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-prev>
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M5 1 1 5l4 4"/>
+                    </svg>
+                    <span class="sr-only">Previous</span>
+                </span>
+            </button>
+            <button type="button" class="absolute top-0 end-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none" data-carousel-next>
+                <span class="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
+                    <svg class="w-4 h-4 text-white dark:text-gray-800 rtl:rotate-180" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 6 10">
+                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="m1 9 4-4-4-4"/>
+                    </svg>
+                    <span class="sr-only">Next</span>
+                </span>
+            </button>`;
+
+    //<!-- Carousel -->
+    const elementWarper = `<div class="relative overflow-hidden rounded-lg" style="height: 555px;">${elements}</div>`;
+    carousel.innerHTML += `${ttl}<div id="carousel" class="relative w-full" data-carousel="static">${
+      elementWarper + indicatorsContent + sliderControls
+    }</div>`;
+  }
+
+  function loadData(title, data) {
+    const projectsSection = document.getElementById("items-content");
+    const content = `
+                <div class="text-center pt-4">
+                    <h1 class="text-3xl uppercase text-4xl font-bold text-center mt-8 text-gray-700">${title}</h1>
+                </div>
+                <div class="w-fit mx-auto grid grid-cols-1 lg:grid-cols-3 md:grid-cols-2 justify-items-center justify-center gap-y-20 gap-x-14 mt-10 mb-5 mx-4 p-4 pb-10 sm:p-10 rounded-xl xl:flex-row bg-gray-200">
+                `;
+    let elements = "";
+    for (let d of data) {
+      const projectHTML = `
+            <section class="">
+                <div class="w-72 bg-gray-900 shadow-md rounded-xl duration-500 contrast-75 brightness-90 hover:scale-110 hover:shadow-xl hover:contrast-100 hover:brightness-100">
+                    <a href="${d.link}" target="_blank" >
+                        <img src="${d.image}" alt="Product" class="h-80 w-72 object-cover rounded-t-xl" />
+                        <div class="px-4 py-3 w-72">
+                            <span class="text-gray-300 mr-3 uppercase text-xs">${d.brand}</span>
+                            <p class="text-lg font-bold text-black text-white truncate block uppercase">${d.title}</p>
+                            <div class="flex items-center">
+                            <p class="text-lg font-semibold cursor-auto my-3 text-blue-400">${d.price}</p>                                          
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </section>
+            `;
+      elements += projectHTML;
+    }
+
+    // Agrega el HTML generado al contenido de la sección
+    projectsSection.innerHTML += content + elements + "</div>"; // close last div
+  }
+
+  function loadContent() {
+    loadData("Cocina", cocina);
+    loadData("Baños", bathroom);
+    loadData("Luces", luces);
+
+    loadCarousel("Cocina", cocina);
+    loadCarousel("Baños", bathroom);
+    loadCarousel("Luces", luces);
+  }
+  loadContent();
+});
---
+++ public/data.js
@@ -0,0 +1,131 @@
+//All data
+const cocina = [
+  {
+    title: "GRIFO COCINA - LIV-PL01",
+    price: "₡ 86,900",
+    brand: "kthinfinite",
+    image:
+      "https://media.nidux.net/pull/800/599/15612/166-product-653198ff0ab26-liv-pl01-01.png",
+    link: "https://www.kthinfiniteshop.com/products/1/liv-01",
+  },
+  {
+    title: "FREGADERO AZUR-7946",
+    price: "₡ 205,900",
+    brand: "kthinfinite",
+    image:
+      "https://media.nidux.net/pull/800/599/15612/373-product-6538308c46efa-azur-7946-01.png",
+    link: "https://www.kthinfiniteshop.com/products/373/azur-7946",
+  },
+];
+
+const bathroom = [
+  {
+    title: "GTRIFO Principal",
+    price: "₡ 30,000",
+    brand: "kthinfinite",
+    image: "https://dpbfouxy1lg2q.cloudfront.net/65956/65956-1.webp",
+    link: "https://www.construplaza.com/P/NjU5NTY=",
+  },
+  {
+    title: "Sanitario Principal",
+    price: "₡ 120,000",
+    brand: "Franz Viegener",
+    image: "https://dpbfouxy1lg2q.cloudfront.net/58334/58334-2.webp",
+    link: "https://www.construplaza.com/Materiales/Ba%C3%B1os/Inodoros/Inodoro-una-pieza-doble-descarga-35-y-48-litros-600-gramos-blanco-Riga-Franz-Viegener-ZT410-PST-BL",
+  },
+  {
+    title: "Sanitario Principal",
+    price: "₡ 60,000",
+    brand: "construplaza",
+    image: "https://dpbfouxy1lg2q.cloudfront.net/64833/64833-1.webp",
+    link: "https://www.construplaza.com/P/NjQ4MzM=",
+  },
+  {
+    title: "LAVATORIO DE CERAMICA",
+    price: "₡ 49,900",
+    brand: "KTHINFINITE",
+    image:
+      "https://media.nidux.net/pull/800/599/15612/468-product-6538412b07f4e-kth-720-01.png",
+    link: "https://www.kthinfiniteshop.com/products/468/kth-720",
+  },
+  {
+    title: "Ducha",
+    price: "₡ 72.950",
+    brand: "Pfister",
+    image:
+      "https://cr.epaenlinea.com/media/catalog/product/6/6/66fe814d-236d-4962-b10a-53c876b877db.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=300&width=300&canvas=300:300",
+    link: "https://cr.epaenlinea.com/ducha-maia-con-lavapies-dos-mandos-cromo-pulido-tipo-pomo-price-pfister.html",
+  },
+  {
+    title: "Grifo secundario",
+    price: "₡ 18.950",
+    brand: "Aqua Nuova",
+    image:
+      "https://cr.epaenlinea.com/media/catalog/product/7/6/763bfc82-fe7d-481a-9e5f-97e302fabdae.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=600&width=300&canvas=600:600",
+    link: "https://cr.epaenlinea.com/grifo-lavamanos-monomando-cuello-bajo-cromo-satinado-tipo-palanca.html",
+  },
+  {
+    title: "Rejilla",
+    price: "₡ 11.995",
+    brand: "epaenlinea",
+    image:
+      "https://cr.epaenlinea.com/media/catalog/product/f/8/f8d57972-2ca6-4e82-b1af-49a4c1f25ad9.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=300&width=300&canvas=300:300",
+    link: "https://cr.epaenlinea.com/rejilla-desague-ba-o-cuadrada-acero-inoxidable-antiolores-e-insectos.html",
+  },
+  {
+    title: "Extractor",
+    price: "₡ 22,000",
+    brand: "construplaza",
+    image: "https://dpbfouxy1lg2q.cloudfront.net/08559/08559-1.webp",
+    link: "https://www.construplaza.com/P/MDg1NTk=",
+  },
+  {
+    title: "PORTA PAPEL",
+    price: "₡ 10,900",
+    brand: "mundoiluminacion",
+    image:
+      "https://mundoiluminacion.com/costa-rica/wp-content/uploads/sites/2/2023/10/PortaPapelHigienicoMundoIluminacion4.png",
+    link: "https://mundoiluminacion.com/costa-rica/producto/porta-papel-higienico-europeo/",
+  },
+  {
+    title: "6 Pack",
+    price: "₡ 20,300",
+    brand: "amazon",
+    image: "https://m.media-amazon.com/images/I/411wxXIaTpL._AC_SL1080_.jpg",
+    link: "https://www.amazon.com/LGSFTBFS-Bathroom-Stainless-Clothes-Kitchen/dp/B0B88H9ZQ2/ref=sr_1_22_sspa?crid=2ECMSFT4WJWQ0&dib=eyJ2IjoiMSJ9.MG0fRrhDIpOrNpoypKmJCuQm5GmYXBH_SudT8nYGxJmDtEJXppAH_D7sebUOEmdzW4Z0B-WRsR7JUII3pfrFBWhCBQuIECh_kt03w8XGXensiY2s2CAz8JdI41paQcEiMZtFnpQiSJb3xIZwq977cSnNSyzF4CNPVTcBKyqo90RhRg9mpoghLlSuz93CBYlqCphtWUt4QAjDM1geZmveOFqilcYDcPVg5Irlx9uMf0cb76pW4-5VW-uDC0wFkAu_0pTpON3Mx_lg0PRAtukK1boeJwL5q8d42NRYeW8eN8g._Yh9QbxfEb7EzKWX_ptir2TL-Bhfacf4dKbCL2_0ns8&dib_tag=se&keywords=GANCHO%2BPARA%2BBA%C3%91O&qid=1712927274&sprefix=gancho%2Bpara%2Bba%C3%B1o%2Caps%2C128&sr=8-22-spons&sp_csd=d2lkZ2V0TmFtZT1zcF9tdGY&th=1",
+  },
+];
+
+const luces = [
+  {
+    title: "LÁMPARA SALA",
+    price: "₡ 162,850",
+    brand: "mundoiluminacion - NOVELT",
+    image:
+      "https://mundoiluminacion.com/costa-rica/wp-content/uploads/sites/2/2023/10/LamparaNovelt2.png",
+    link: "https://mundoiluminacion.com/costa-rica/producto/lampara-novelt/",
+  },
+  {
+    title: "Luz empotrada redonda",
+    price: "₡ 6,500.00",
+    brand: "construplaza",
+    image: "https://dpbfouxy1lg2q.cloudfront.net/59162/59162-1.webp",
+    link: "https://www.construplaza.com/Materiales/Iluminaci%C3%B3n-y-Ventilaci%C3%B3n/L%C3%A1mparas/Lampara-empotrada-redonda-LED-12W-100-240V-4000K-blanco-Bucaramanga-Tecnolite-12YDLED430MV40B",
+  },
+  {
+    title: "APLIQUE FIG",
+    price: "₡ 28,800",
+    brand: "mundoiluminacion",
+    image:
+      "https://mundoiluminacion.com/costa-rica/wp-content/uploads/sites/2/2023/10/ApliqueFig2.png",
+    link: "https://mundoiluminacion.com/costa-rica/producto/aplique-fig/",
+  },
+  {
+    title: "Luz exterior",
+    price: "₡ 22,374",
+    brand: "mundoiluminacion",
+    image:
+      "https://mundoiluminacion.com/costa-rica/wp-content/uploads/sites/2/2023/10/ApliqueRussian3.png",
+    link: "https://mundoiluminacion.com/costa-rica/producto/aplique-russian/",
+  },
+];
